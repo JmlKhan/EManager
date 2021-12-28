c5692683c7b32f4a784d9ba4fb61a4bea07fb1b7
JmlKhan <jmlkhon17@gmail.com>
2021-12-28 11:50:30 +0500

    creating models

---
+++ EManager/Model/Department.cs
@@ -0,0 +1,8 @@
+﻿namespace EManager.Model
+{
+    public class Department
+    {
+        public int DepartmentId { get; set; }
+        public string DepartmentName { get; set; }
+    }
+}
--- EManager/Program.cs
+++ EManager/Program.cs
@@ -1,13 +1,36 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.AspNetCore.Http.Json;
+using Newtonsoft.Json.Serialization;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
+var builder = WebApplication.CreateBuilder(args);
+//Json serializer
+builder.Services.Configure<JsonOptions>(options =>
+{
+    options.SerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+    options.SerializerOptions.WriteIndented = true;
+    options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+    options.SerializerOptions.Converters.Add(new JsonStringEnumConverter(JsonNamingPolicy.CamelCase));
+});
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+//enable cors
+builder.Services.AddCors(c =>
+{
+    c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+});
+
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
